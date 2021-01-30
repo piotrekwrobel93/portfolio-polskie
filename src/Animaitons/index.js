545009079f3610458464226e1cbd5c8d1138df79
piwkodev <piotrwrobeldev@gmail.com>
2021-01-30 00:18:02 +0000

    media queries for homepage update

--- src/Animaitons/index.js
+++ src/Animaitons/index.js
@@ -60,7 +60,7 @@ window.addEventListener("DOMContentLoaded", function() {
                     gsap.to( window,{
                         scrollTo: {y: 0},
                         delay: 0,
-                        duration: 2
+                        duration: 0.5
                     }),
                     gsap.to("#home", {
                         x:"0%",
